2d7fbc07f3f90fe9f37dda501a327f4b19117cd1
AtifAziz600 <feralligatrfahim630@gmail.com>
2026-08-01 22:05:25 +0600

    s

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,1 +1,15 @@
-import axios from " axios\;
+import axios from "axios";
+
+export const axiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+});
+
+axiosInstance.interceptors.request.use((config) => {
+  if (typeof window !== "undefined") {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+  }
+  return config;
+});
